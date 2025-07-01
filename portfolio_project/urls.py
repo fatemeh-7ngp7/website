aac62ec44eb56042ddb1d787c6375e628d9a81f9
fatemeh-7ngp7 <najafgholipoorfatemeh@gmail.com>
2025-07-01 08:36:42 -0700

    Updated with i18n URL patterns

--- portfolio_project/urls.py
+++ portfolio_project/urls.py
@@ -18,11 +18,21 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
 
+# URLs that don't need language prefix
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('portfolio.urls')),
+    path('i18n/', include('django.conf.urls.i18n')),  # For language switching
 ]
 
+# URLs that will have language prefix (e.g., /en/, /fa/)
+urlpatterns += i18n_patterns(
+    path('', include('portfolio.urls')),
+    prefix_default_language=False  # Don't add /en/ for English
+)
+
+# Serve media files during development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
